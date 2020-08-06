b4d8ef322e44d2b9cef94cc9ba64d7e02a64a898
golamc2020 <golamc815@gmail.com>
2020-08-06 14:21:13 -0400

    ooo

---
+++ src/Members.java
@@ -0,0 +1,45 @@
+
+public class Members {
+
+	
+	
+	private int memberID;
+	private String memberName;
+	
+	
+	
+	public int getId() {
+		return memberID;
+	}
+
+
+
+	public void setId(int id) {
+		this.memberID = memberID;
+	}
+
+
+
+	public String getName() {
+		return memberName;
+	}
+
+
+
+	public void setName(String name) {
+		this.memberName = memberName;
+	}
+
+
+
+	public void checkIn(Club club) {
+		
+		
+		
+		System.out.println();
+	}
+	
+	
+	
+	
+}
