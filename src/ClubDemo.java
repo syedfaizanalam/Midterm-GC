ad9a339d00c157fabf2fbff508c75c97eca83ed5
Kavitha <kavithaa434@gmail.com>
2020-08-07 10:32:51 -0400

    fourth commit

--- src/ClubDemo.java
+++ src/ClubDemo.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-
-
 public class ClubDemo {
-	//private static Path filePath = Paths.get("C:\\Users\\Kavitha\\eclipse-workspace\\lab17\\.settings\\country.txt");
+	private static Path filePath = Paths.get("members.txt");
+	//private static Path filePath2 = Paths.get("clubnames.txt");
 	private static List<Members> members = new ArrayList<>();
 	//private static List<Members> list=new ArrayList<>();
 	public static void main(String[] args) {
@@ -63,9 +65,63 @@ public class ClubDemo {
 					}
 				}
 			}
+			else if(userInput==1) {
+				listMembers();
+			}
 			
 			} 
 	}
+	public static List<Members> readFile() {
+		try {
+			List<String> lines = Files.readAllLines(filePath);
+			Collections.sort(lines, String.CASE_INSENSITIVE_ORDER);
+			List<Members> members = new ArrayList<>();
+			for (String line : lines) {
+				String[] parts = line.split(",");
+				if(members instanceof SingleClubMembers) {
+				int memberId = Integer.parseInt(parts[0]);
+				String memberName = parts[1];
+				String clubName = parts[2];
+				members.add(new SingleClubMembers(memberId, memberName, clubName));
+			}else {
+				int memberId = Integer.parseInt(parts[0]);
+				String memberName = parts[1];
+				int points =Integer.parseInt(parts[2]);
+				members.add(new MultiClubMembers(memberId, memberName, points));
+			}
+				
+			}
+
+			return members;
+		} catch (IOException e) {
+			System.out.println("Unable to read file.");
+			return new ArrayList<>();
+		}
+	}
+	public static void listMembers() {
+		members = readFile();
+		System.out.printf("%-15s%-15s%-15s\n", "Member Id", "Name", "Club Name/Points");
+		System.out.printf("%-15s%-15s%-15s\n", "=========", "====", "================");
+		for (int i=0;i<members.size();i++){
+			
+			if (members.get(i) instanceof SingleClubMembers)
+			{
+				System.out.printf("%-15d%-15s%-15s",members.get(i).getId(),
+				           members.get(i).getName(),
+						           ((SingleClubMembers)members.get(i)).getClubName());
+			}
+			else
+			{
+				System.out.printf("%-15d%-15s%-15d",members.get(i).getId(),
+				           members.get(i).getName(),
+				           ((MultiClubMembers)members.get(i)).getPoints());	
+			}
+		}
+			
+
+		}
+		
+	
 	
 
 }
