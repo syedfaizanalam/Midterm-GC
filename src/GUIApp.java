91515ede53cf49e34f32d9c1ac54c92cfd2216c6
syedfaizanalam <43822557+syedfaizanalam@users.noreply.github.com>
2020-08-07 15:15:20 -0400

    update

---
+++ src/GUIApp.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GUIApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame appFrame = new JFrame();
+		appFrame.setSize(400, 250);
+		appFrame.setTitle("ABC Gym");
+		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		System.out.println(appFrame);
+		
+	}
+
+}
