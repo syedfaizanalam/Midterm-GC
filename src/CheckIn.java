8c7a19855f29c5fbb74dfd52f8996bd91a6d54ea
syedfaizanalam <43822557+syedfaizanalam@users.noreply.github.com>
2020-08-10 11:14:26 -0400

    update

--- src/CheckIn.java
+++ src/CheckIn.java
@@ -14,12 +14,12 @@ public class CheckIn extends JFrame implements ActionListener {
 	private JLabel memberID; // Label for hourly salary
 	private JLabel clubName; // Label for yearly salary
 	private JTextField ID; // Displays hourly salary
-<<<<<<< HEAD
+
 	private JTextField enterClubName; // Displays yearly salary
 	private JButton checkIn;
-=======
+
 	private JTextField ClubName; // Displays yearly salary
->>>>>>> 4ea1705a4d492042d7312a6e1b6a34214764ed1f
+
 
 	public CheckIn() {
 
